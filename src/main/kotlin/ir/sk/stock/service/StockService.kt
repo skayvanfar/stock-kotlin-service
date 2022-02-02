5bbe7afb1ec03b276d0c29d65e7f079c477c48d2
Saeed kayvanfar <kayvanfar.sj@gamil.com>
2022-02-02 23:54:29 +0330

    add StockService

---
+++ src/main/kotlin/ir/sk/stock/service/StockService.kt
@@ -0,0 +1,6 @@
+package ir.sk.stock.service
+
+/**
+ * Created by sad.kayvanfar on 2/2/2022
+ */
+interface StockService 
---
+++ src/main/kotlin/ir/sk/stock/service/StockServiceImpl.kt
@@ -0,0 +1,4 @@
+package ir.sk.stock.service
+
+class StockServiceImpl : StockService {
+}
