2987e77014230eb6ec1a07179caeee8b31c20d20
Saeed kayvanfar <kayvanfar.sj@gamil.com>
2022-02-02 23:38:03 +0330

    add Stock model

---
+++ src/main/kotlin/ir/sk/stock/model/Stock.kt
@@ -0,0 +1,11 @@
+package ir.sk.stock.model
+
+import java.math.BigDecimal
+import java.time.Instant
+
+class Stock {
+    private val id: Long? = null
+    private val name: String? = null
+    private val currentPrice: BigDecimal? = null
+    private val lastUpdate: Instant? = null
+}
